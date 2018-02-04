563b8ad7e245804d70b92040aee878d1edeb367c
Adil Sadik <sparky.005@gmail.com>
2018-02-04 12:22:42 -0500

    remove now-unneeded helper functions from main app

--- efefal/efefal.py
+++ efefal/efefal.py
@@ -1,11 +1,7 @@
 import inspect
-import json
-import itertools
 from flask import Flask
 from flask import render_template
 from flask import Blueprint
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
 from efefal.searchclient import SearchClient
 
 bp = Blueprint('efefal', __name__)
@@ -20,32 +16,6 @@ def create_app(config=None):
 
 app = create_app()
 client = SearchClient()
-
-"""Helper functions"""
-def timestamp_sort(hits):
-    hits = sorted(hits, key=lambda d : d['@timestamp'])
-    return hits
-
-def calculate_totals(result):
-    totals = {
-                "ok": 0,
-                "failures": 0,
-                "unreachable": 0,
-                "changed": 0,
-                "skipped": 0,
-            }
-    for host_result in result.keys():
-        for key, value in result[host_result].items():
-            totals[key] += value
-    return totals
-
-def remove_tasklist_duplicates(task_list):
-    results = []
-    for name, group in itertools.groupby(sorted(task_list,
-                                                key=lambda d : d['ansible_task']),
-                                                key=lambda d : d['ansible_task']):
-        results.append(next(group))
-    return results
 
 @bp.route('/')
 def playbook_index():
