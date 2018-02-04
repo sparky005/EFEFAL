490a64c6e0e978c5396369feaaba8ece6ebbc25c
Adil Sadik <sparky.005@gmail.com>
2018-02-04 11:31:33 -0500

    Tests for runs

--- tests/test_efefal.py
+++ tests/test_efefal.py
@@ -17,3 +17,14 @@ def test_playbook_index(app):
     assert b'test2.yml' in rv.data
     assert b'test3.yml' in rv.data
 
+def test_runs(app):
+    rv = app.get('/runs/test3.yml')
+    assert b'SessionID' in rv.data
+    assert b'Timestamp' in rv.data
+    assert b'Playbook' in rv.data
+    assert b'Ok' in rv.data
+    assert b'Failed' in rv.data
+    assert b'Unreachable' in rv.data
+    assert b'Changed' in rv.data
+    assert b'Skipped' in rv.data
+    assert b'8262ba60-0930-11e8-8eaf-c48e8ff31cf7' in rv.data
